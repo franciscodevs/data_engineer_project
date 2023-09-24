013161ac508acb0290314a6baf0b2859da662194
Juacoos <joaojoeo@gmail.com>
2023-09-24 12:35:41 -0300

    Unificado en etl.py, terminado de Docker

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,27 @@
-# Usa la imagen oficial de PostgreSQL como base
-FROM postgres:13
+# Utiliza una imagen de Python 3.11
+FROM python:3.11
 
-# Define variables de entorno para configurar la base de datos
-ENV POSTGRES_DB=de-prueba
-ENV POSTGRES_USER=root
-ENV POSTGRES_PASSWORD=root
+# Copia el archivo requirements.txt al directorio /app en el contenedor
+COPY requirements.txt /app/requirements.txt
+COPY data /app/data
+COPY config.ini /app/config.ini
 
-# Copia scripts SQL personalizados al contenedor (opcional)
-#COPY init.sql /docker-entrypoint-initdb.d/
-EXPOSE 5432
+# Establece el directorio de trabajo
+WORKDIR /app
+
+# Crea y activa un entorno virtual de Python
+RUN python -m venv env
+SHELL ["/bin/bash", "-c"]
+RUN source env/bin/activate
+
+# Instala las dependencias desde el archivo requirements.txt
+RUN pip install -r requirements.txt
+
+# Copia otro archivo Python a /app
+COPY etl.py /app/etl.py
+
+# Establece una variable de entorno con el nombre del servicio de la base de datos
+ENV DB_HOST=data_engineer_project-database-1
+
+# Ejecuta el otro script después de la instalación de las dependencias
+CMD ["python", "etl.py"]
--- etl.py
+++ etl.py
@@ -1,6 +1,10 @@
+import psycopg2
 import pandas as pd
-# from requests.exceptions import RequestException
+import sqlalchemy as sa
+from configparser import ConfigParser
+import os
 
+# Función para obtención de datos desde archivo csv
 def get_data(path):
   try:
     with open(path,"r",encoding="utf-8") as archivo:
@@ -11,7 +15,7 @@ def get_data(path):
     print("Error", e)
   return df
 
-# función de guardado
+# función de guardado como csv
 def save_data_csv(df,path,name):
   try:
     df.to_csv(f"{path}/{name}")
@@ -19,12 +23,74 @@ def save_data_csv(df,path,name):
   except Exception as e:
     print("Error",e)
 
+# conección con SQLAlchemy
+def connect_sqlalchemy(config_file_path="config.ini", section="postgres"):
 
+  # Comprobar si el archivo de configuración existe
+  if not os.path.exists(config_file_path):
+    raise FileNotFoundError(f"El archivo de configuración '{config_file_path}' no existe.")
+
+  # Leer la configuración desde el archivo INI
+  config = ConfigParser()
+  config.read(config_file_path)
+  conn_data = config[section]
+
+  # Obtener los parámetros de conexión
+  host = conn_data.get("host")
+  port = conn_data.get("port")
+  database = conn_data.get("database")
+  user = conn_data.get("user")
+  password = conn_data.get("password")
+
+  url = f"postgresql://{user}:{password}@{host}:{port}/{database}"
+
+  # Establecer la conexión a la base de datos PostgreSQL
+  try:
+    conn = sa.create_engine(url)
+
+    
+  except Exception as e:
+    print("Error de conexión", e)
+  return conn
+
+# Conección con psycopg2
+def connect_psycopg2(config_file_path="config.ini", section="postgres"):
+  # Comprobar si el archivo de configuración existe
+  if not os.path.exists(config_file_path):
+    raise FileNotFoundError(f"El archivo de configuración '{config_file_path}' no existe.")
+  # Leer la configuración desde el archivo INI
+  config = ConfigParser()
+  config.read(config_file_path)
+  conn_data = config[section]
+
+  try:
+    conn=psycopg2.connect(
+      host=conn_data.get("host"),
+      port=conn_data.get("port"),
+      user=conn_data.get("user"),
+      password=conn_data.get("password"),
+      database=conn_data.get("database")
+    )
+    cur=conn.cursor()
+  except Exception as e:
+    print("Error en psycopg2", e)
+  
+  #Retornamos cursor
+  return cur
+
+# función para ejecutar un query
+def query(cursor, query):
+  cursor.execute(query)
+  rs = cursor.fetchall()
+  # Printeamos los resultados
+  for r in rs:
+    print(r)
+
+# Función principal
 def etl():
     
-
+  # Obtenemos los datos
   df = get_data("data/raw/sales_data.csv")
-  # get_data("data/raw/sales_data.csv")
 
   # Arreglo columna Product_ean
   df['Product_ean'] = df['Product_ean'].astype(str).apply(lambda x: x.replace('.0',''))
@@ -42,13 +108,33 @@ def etl():
   ).drop("Product",axis=1)
 
   # Creamos columna Google_Maps
-  df["Google_Maps"] = "https://www.google.com/maps/place/" + df['Purchase Address'].apply(lambda x:x.replace(' ', '+')) 
+  df["Google_Maps"] = "https://www.google.com/maps/place/" + df['Purchase Address'].apply(lambda / x:x.replace(' ', '+')) 
 
-  #guardamos
+  """
+  En este apartado guardamos las tablas como archivos csv en nuestro 'datalake'
+  """
   save_data_csv(df,"data/processed","sales_data_clean.csv")
   save_data_csv(df_prod,"data/processed","products_clean.csv")
   
   
+  # Obtenemos los datos limpios
+  df_sales = get_data("data/processed/sales_data_clean.csv")
+  df_prod = get_data("data/processed/products_clean.csv")
+  
+  # Creamos la conexión con SQLAlchemy
+  try:
+    engine = connect_sqlalchemy("config.ini","postgres")
+  except Exception as e:
+    raise e
+  
+  # Cargamos la tabla en la base de datos
+  df_sales.to_sql("sales_order",engine,if_exists='replace', index=False)
+  df_prod.to_sql("products",engine,if_exists='replace', index=False)
+  """
+  # Query de productos
+  cur = connect_psycopg2()
+  print("PRODUCTOS: ")
+  query(cur, "Select * from products limit 5;")
+  """
 if __name__ == "__main__":
     etl()
-
